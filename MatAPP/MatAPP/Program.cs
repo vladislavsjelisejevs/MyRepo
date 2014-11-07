630f6ecaf5ab4c86260b0adf23e9c0aaeb4280df
Vladislavs <regios@inbox.lv>
2014-11-07 14:22:37 +0200

    Papildinaju programmu, lai ta varetu izskaitlotu x un y reizinajumu

--- MatAPP/MatAPP/Program.cs
+++ MatAPP/MatAPP/Program.cs
@@ -9,11 +9,13 @@ namespace MatAPP
     {
         static void Main(string[] args)
         {
-            int x, y;
+            int x, y, z;
             string s;
             Console.WriteLine("Ievadi divus skaitlus: ");
             Console.Write("x= "); s = Console.ReadLine(); x = int.Parse(s);
             Console.Write("y= "); s = Console.ReadLine(); y = int.Parse(s);
+            z = x * y;
+            Console.WriteLine("x*y= " + z.ToString());
             Console.ReadLine();
         }
     }
